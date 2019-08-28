110c173f5fe04098ff5e267d6301b015879104d5
ARK Deployer <support@ark.io>
2019-08-28 00:17:08 +0000

    chore: prepare new network config 🎉

--- packages/core/bin/config/devnet/plugins.js
+++ packages/core/bin/config/devnet/plugins.js
@@ -15,9 +15,9 @@ module.exports = {
         maxTransactionsPerSender: process.env.CORE_TRANSACTION_POOL_MAX_PER_SENDER || 300,
         allowedSenders: [],
         dynamicFees: {
-            enabled: true,
-            minFeePool: 1000,
-            minFeeBroadcast: 1000,
+            enabled: false,
+            minFeePool: 3000,
+            minFeeBroadcast: 3000,
             addonBytes: {
                 transfer: 100,
                 secondSignature: 250,
@@ -27,7 +27,7 @@ module.exports = {
                 ipfs: 250,
                 timelockTransfer: 500,
                 multiPayment: 500,
-                delegateResignation: 400000,
+                delegateResignation: 100,
             },
         },
     },
